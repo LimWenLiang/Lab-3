7f9f73bf35932d076fe84e24a01055cb0d4a1526
LimWenLiang <lwiang98@gmail.com>
2021-01-10 02:05:31 +0800

    google map

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoder/geocoder.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -8,9 +12,17 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  double latitude, longitude, restlat, restlong;
-  String _homeloc = "Searching...";
+  double screenHeight, screenWidth, latitude, longitude, restlat, restlong;
+  String titleCenter = "Loading Map...";
+  String _homeloc = "";
+  String _latLng = "";
   Position _currentPosition;
+  Completer<GoogleMapController> _controller = Completer();
+  GoogleMapController gmcontroller;
+  CameraPosition _home;
+  MarkerId markerId1 = MarkerId("12");
+  Set<Marker> markers = Set();
+  CameraPosition _userpos;
 
   @override
   void initState() {
@@ -20,17 +32,46 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text('Main Screen'),
       ),
       body: Center(
         child: Container(
+            alignment: Alignment.topCenter,
             child: Padding(
                 padding: EdgeInsets.all(10),
                 child: SingleChildScrollView(
                     child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
                   children: [
+                    latitude == null || longitude == null
+                        ? Container(
+                            child: Text(titleCenter,
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold)),
+                          )
+                        : Container(
+                            height: screenHeight - 220,
+                            width: screenWidth - 10,
+                            child: GoogleMap(
+                                mapType: MapType.normal,
+                                initialCameraPosition: CameraPosition(
+                                    target: LatLng(latitude, longitude),
+                                    zoom: 17),
+                                markers: markers.toSet(),
+                                onMapCreated: (controller) {
+                                  _controller.complete(controller);
+                                },
+                                onTap: (newLatLng) {
+                                  _loadLoc(newLatLng, setState);
+                                }),
+                          ),
+                    SizedBox(height: 10),
+                    Text(_latLng),
                     Text(_homeloc),
                   ],
                 )))),
@@ -38,7 +79,7 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
-  void _getLocation() {
+  Future<void> _getLocation() async {
     try {
       final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
       geolocator
@@ -51,6 +92,13 @@ class _MainScreenState extends State<MainScreen> {
           var addresses =
               await Geocoder.local.findAddressesFromCoordinates(coordinates);
           setState(() {
+            print(_currentPosition.latitude.toString() +
+                "/" +
+                _currentPosition.longitude.toString());
+            _latLng = "Latitude: " +
+                _currentPosition.latitude.toStringAsFixed(4) +
+                "\nLongitude: " +
+                _currentPosition.longitude.toStringAsFixed(4);
             var first = addresses.first;
             _homeloc = first.addressLine;
             if (_homeloc != null) {
@@ -66,5 +114,58 @@ class _MainScreenState extends State<MainScreen> {
     } catch (exception) {
       print(exception.toString());
     }
+  }
+
+  void _loadLoc(LatLng loc, setState) async {
+    setState(() {
+      print("insetstate");
+      markers.clear();
+      latitude = loc.latitude;
+      longitude = loc.longitude;
+      _getLocationfromlatlng(latitude, longitude, setState); //get new address
+      _home = CameraPosition(
+        target: loc,
+        zoom: 17,
+      );
+      markers.add(Marker(
+        markerId: markerId1,
+        position: LatLng(latitude, longitude),
+        infoWindow: InfoWindow(
+          title: 'New Location',
+          snippet: 'New Delivery Location',
+        ),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
+      ));
+    });
+    _userpos = CameraPosition(
+      target: LatLng(latitude, longitude),
+      zoom: 14.4746,
+    );
+    _newhomeLocation();
+  }
+
+  _getLocationfromlatlng(double lat, double lng, setState) async {
+    final Geolocator geolocator = Geolocator()
+      ..placemarkFromCoordinates(lat, lng);
+    _currentPosition = await geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    //debugPrint('location: ${_currentPosition.latitude}');
+    final coordinates = new Coordinates(lat, lng);
+    var addresses =
+        await Geocoder.local.findAddressesFromCoordinates(coordinates);
+    var first = addresses.first;
+    setState(() {
+      _homeloc = first.addressLine;
+      if (_homeloc != null) {
+        latitude = lat;
+        longitude = lng;
+        return;
+      }
+    });
+  }
+
+  Future<void> _newhomeLocation() async {
+    gmcontroller = await _controller.future;
+    gmcontroller.animateCamera(CameraUpdate.newCameraPosition(_home));
   }
 }
