ef338bc8373aa27b33ab7e092bafe834bb60bcd7
LimWenLiang <lwiang98@gmail.com>
2021-01-10 03:47:11 +0800

    google map

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoder/geocoder.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -15,14 +13,14 @@ class _MainScreenState extends State<MainScreen> {
   double screenHeight, screenWidth, latitude, longitude, restlat, restlong;
   String titleCenter = "Loading Map...";
   String _homeloc = "";
-  String _latLng = "";
+  String _latitude = "";
+  String _longitude = "";
   Position _currentPosition;
   Completer<GoogleMapController> _controller = Completer();
   GoogleMapController gmcontroller;
   CameraPosition _home;
   MarkerId markerId1 = MarkerId("12");
   Set<Marker> markers = Set();
-  CameraPosition _userpos;
 
   @override
   void initState() {
@@ -35,48 +33,95 @@ class _MainScreenState extends State<MainScreen> {
     screenHeight = MediaQuery.of(context).size.height;
     screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Main Screen'),
-      ),
-      body: Center(
-        child: Container(
-            alignment: Alignment.topCenter,
-            child: Padding(
-                padding: EdgeInsets.all(10),
-                child: SingleChildScrollView(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    latitude == null || longitude == null
-                        ? Container(
-                            child: Text(titleCenter,
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold)),
-                          )
-                        : Container(
-                            height: screenHeight - 220,
-                            width: screenWidth - 10,
-                            child: GoogleMap(
-                                mapType: MapType.normal,
-                                initialCameraPosition: CameraPosition(
-                                    target: LatLng(latitude, longitude),
-                                    zoom: 17),
-                                markers: markers.toSet(),
-                                onMapCreated: (controller) {
-                                  _controller.complete(controller);
-                                },
-                                onTap: (newLatLng) {
-                                  _loadLoc(newLatLng, setState);
-                                }),
-                          ),
-                    SizedBox(height: 10),
-                    Text(_latLng),
-                    Text(_homeloc),
-                  ],
-                )))),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Location Detector'),
+        ),
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: SingleChildScrollView(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                latitude == null || longitude == null
+                    ? Container(
+                        child: Text(titleCenter,
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                      )
+                    : Column(children: [
+                        Container(
+                          height: screenHeight - 220,
+                          width: screenWidth - 10,
+                          child: GoogleMap(
+                              mapType: MapType.normal,
+                              initialCameraPosition: CameraPosition(
+                                  target: LatLng(latitude, longitude),
+                                  zoom: 17),
+                              markers: markers.toSet(),
+                              onMapCreated: (controller) {
+                                _controller.complete(controller);
+                              },
+                              onTap: (newLatLng) {
+                                _loadLoc(newLatLng, setState);
+                              }),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            RichText(
+                                text: TextSpan(
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 14),
+                                    children: [
+                                  TextSpan(
+                                      text: "Latitude: ",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(
+                                    text: _latitude,
+                                  ),
+                                ])),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            RichText(
+                                text: TextSpan(
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 14),
+                                    children: [
+                                  TextSpan(
+                                      text: "Logitude: ",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(
+                                    text: _longitude,
+                                  ),
+                                ])),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Column(children: [
+                              Text("Address: ",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold)),
+                            ]),
+                          ],
+                        ),
+                        Text(_homeloc,
+                            style: TextStyle(
+                              fontSize: 14,
+                            )),
+                      ])
+              ],
+            )),
+          ),
+        ));
   }
 
   Future<void> _getLocation() async {
@@ -92,13 +137,8 @@ class _MainScreenState extends State<MainScreen> {
           var addresses =
               await Geocoder.local.findAddressesFromCoordinates(coordinates);
           setState(() {
-            print(_currentPosition.latitude.toString() +
-                "/" +
-                _currentPosition.longitude.toString());
-            _latLng = "Latitude: " +
-                _currentPosition.latitude.toStringAsFixed(4) +
-                "\nLongitude: " +
-                _currentPosition.longitude.toStringAsFixed(4);
+            _latitude = _currentPosition.latitude.toStringAsFixed(7);
+            _longitude = _currentPosition.longitude.toStringAsFixed(7);
             var first = addresses.first;
             _homeloc = first.addressLine;
             if (_homeloc != null) {
@@ -132,15 +172,11 @@ class _MainScreenState extends State<MainScreen> {
         position: LatLng(latitude, longitude),
         infoWindow: InfoWindow(
           title: 'New Location',
-          snippet: 'New Delivery Location',
+          snippet: 'New Selected Location',
         ),
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
       ));
     });
-    _userpos = CameraPosition(
-      target: LatLng(latitude, longitude),
-      zoom: 14.4746,
-    );
     _newhomeLocation();
   }
 
@@ -155,6 +191,8 @@ class _MainScreenState extends State<MainScreen> {
         await Geocoder.local.findAddressesFromCoordinates(coordinates);
     var first = addresses.first;
     setState(() {
+      _latitude = lat.toStringAsFixed(7);
+      _longitude = lng.toStringAsFixed(7);
       _homeloc = first.addressLine;
       if (_homeloc != null) {
         latitude = lat;
